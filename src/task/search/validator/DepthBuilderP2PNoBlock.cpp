19d94743466b20aaf626264b56a7fe352adc17a2
kilel <ki.lel@yandex.ru>
2014-05-19 03:36:55 +0400

    fix: fixed bug of starting with grade < 10 (unpredictable ANY_SOURCE comes as source of bug message).

--- src/task/search/validator/DepthBuilderP2PNoBlock.cpp
+++ src/task/search/validator/DepthBuilderP2PNoBlock.cpp
@@ -94,13 +94,10 @@ namespace dgmark {
 		if (tgtRank == rank) {
 			tgtDepth = depth[tgtLocal];
 		} else {
-			if (tgtRank == size) {
-				printf("%d: [%ld]\n", rank, tgtVertex);
-			}
 			Request sendReq = comm->Isend(&tgtLocal, 1, VERTEX_TYPE, tgtRank, LOCAL_SEND_TAG);
 			Request recvReq = comm->Irecv(&tgtDepth, 1, VERTEX_TYPE, tgtRank, DEPTH_SEND_TAG);
 
-			while (!sendReq.Test() || !recvReq.Test()) {
+			while (!recvReq.Test()) {
 				synchAction();
 			}
 
@@ -116,8 +113,15 @@ namespace dgmark {
 		startRecv();
 
 		if (isRecvActive && recvRequest.Test(status)) {
-			//printf("%d: write depth (%d)\n", rank, status.Get_source());
-			sendVertex(depth[requestedVertex], status.Get_source(), DEPTH_SEND_TAG);
+			//printf("%d: write depth [ %ld] to %d\n", rank, depth[requestedVertex], status.Get_source());
+			if (status.Get_source() != ANY_SOURCE) {
+				//It really had happened to come here!
+				comm->Isend(&depth[requestedVertex],
+					1,
+					VERTEX_TYPE,
+					status.Get_source(),
+					DEPTH_SEND_TAG);
+			}
 			isRecvActive = false;
 		}
 
